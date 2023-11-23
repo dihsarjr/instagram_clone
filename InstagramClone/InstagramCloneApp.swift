3b13d19745a635706e29cb7668893d56f0b89f15
dihsarjr <mohammed1rashid786@gmail.com>
2023-11-23 18:56:56 +0530

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Mohammed Rashid on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
