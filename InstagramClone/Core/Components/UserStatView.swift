947773b039267f6aaa38701492432ff05d53ca05
dihsarjr <mohammed1rashid786@gmail.com>
2023-11-23 20:48:07 +0530

    profile_view_completed

---
+++ InstagramClone/Core/Components/UserStatView.swift
@@ -0,0 +1,18 @@
+//
+//  UserStatView.swift
+//  InstagramClone
+//
+//  Created by Mohammed Rashid on 23/11/23.
+//
+
+import SwiftUI
+
+struct UserStatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserStatView()
+}
---
+++ InstagramClone/Core/TabBar/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  InstagramClone
+//
+//  Created by Mohammed Rashid on 23/11/23.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
