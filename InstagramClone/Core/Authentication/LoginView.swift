888fd00664fd4915bce7b7746d39f919f5f84c4b
dihsarjr <mohammed1rashid786@gmail.com>
2023-11-25 17:04:53 +0530

    sign_up_completed

---
+++ InstagramClone/Core/Authentication/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  InstagramClone
+//
+//  Created by Mohammed Rashid on 25/11/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
