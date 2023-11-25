59dce9e30b23b2952b11a1b7a1ab13f3eea22fd3
dihsarjr <mohammed1rashid786@gmail.com>
2023-11-25 15:57:24 +0530

    search and feed

---
+++ InstagramClone/Core/Feed/FeedCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedCell.swift
+//  InstagramClone
+//
+//  Created by Mohammed Rashid on 25/11/23.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedCell()
+}
---
+++ InstagramClone/Core/Feed/FeedView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedView.swift
+//  InstagramClone
+//
+//  Created by Mohammed Rashid on 25/11/23.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedView()
+}
